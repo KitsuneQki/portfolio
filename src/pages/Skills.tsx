ac43c9382a87899b1133a51d913ff4d483cb97ba
Kevin Zerpa <99708401+KitsuneQki@users.noreply.github.com>
2023-05-29 19:01:16 -0400

    skills page update

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -16,6 +16,8 @@ import {
   SiVisualstudiocode,
   SiGraphql,
   SiPostman,
+  SiTailwindcss,
+  SiNextdotjs,
 } from "react-icons/si";
 
 const Skills = () => {
@@ -28,65 +30,94 @@ const Skills = () => {
           </div>
           <div className="pl-8 py-4">
             <h1 className="pb-3 fs-5 text-start text-white">Languages</h1>
-            <div className="d-flex">
-              <p className="pl-4 py-2 px-4">
+            <div className="d-flex justify-content-start flex-wrap">
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <IoLogoHtml5 className="fs-1" />
-              </p>
-              <p className="pl-4 py-2 px-4 col-2">
+                <p className="fw-bold mt-2">HTML</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <IoLogoCss3 className="fs-1" />
-              </p>
-              <p className="pl-4 py-2 px-4 col-2">
+                <p className="fw-bold mt-2">CSS</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiPython className="fs-1" />
-              </p>
-              <p className="pl-4 py-2 px-4 col-2">
+                <p className="fw-bold mt-2">Python</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <DiJavascript1 className="fs-1" />
-              </p>
-              <p className="pl-4 py-2 px-4 col-2">
+                <p className="fw-bold mt-2">JavaScript</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiTypescript className="fs-1" />
-              </p>
+                <p className="fw-bold mt-2">TypeScript</p>
+              </div>
+            </div>
+          </div>
+          <div className="pl-8 py-4">
+            <h1 className="pb-3 fs-5 text-start text-white">Libraries</h1>
+            <div className="d-flex justify-content-start flex-wrap">
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
+                <FaReact className="fs-1" />
+                <p className="fw-bold mt-2">React.js</p>
+              </div>
             </div>
           </div>
           <div className="pl-8 py-4">
             <h1 className="pb-3 fs-5 text-start text-white">Frameworks</h1>
-            <div className="d-flex">
-              <p className="pl-4 py-2 px-4 col-2">
-                <FaReact className="fs-1" />
-              </p>
-              <p className="pl-4 py-2 px-4">
+            <div className="d-flex justify-content-start flex-wrap">
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
+                <SiNextdotjs className="fs-1" />
+                <p className="fw-bold mt-2">Next.js</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiBootstrap className="fs-1" />
-              </p>
+                <p className="fw-bold mt-2">Bootstrap</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
+                <SiTailwindcss className="fs-1" />
+                <p className="fw-bold mt-2">Tailwind CSS</p>
+              </div>
             </div>
           </div>
           <div className="pl-8 py-4">
             <h1 className="pb-3 fs-5 text-start text-white">Other tools</h1>
-            <div className="row">
-              <p className="pl-4 py-4 px-4 col-2">
+            <div className="d-flex justify-content-start flex-wrap">
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiVisualstudiocode className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">Visual Studio Code</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiGit className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">Git</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiWebpack className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">Webpack</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiSass className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">SASS</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiGraphql className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">GraphQL</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiPostman className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">Postman</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiNodedotjs className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">Node.js</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiMaterialui className="fs-1" />
-              </p>
-              <p className="pl-4 py-4 px-4 col-2">
+                <p className="fw-bold mt-2">Material-UI</p>
+              </div>
+              <div className="mx-4 my-4 col-2 d-flex flex-column justify-content-center align-items-center">
                 <SiStyledcomponents className="fs-1" />
-              </p>
+                <p className="fw-bold mt-2">Styled Components</p>
+              </div>
             </div>
           </div>
         </div>
